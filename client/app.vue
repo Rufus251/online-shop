ac2d25d115ac140dc183f003aa07fbb6520b8de6
Rufus251 <vladimerrybalkin@gmail.com>
2023-06-04 20:05:10 +1000

    Nuxt

---
+++ client/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NuxtPage></NuxtPage>
+
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      isAuth: useState('isAuth', () => true)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ client/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li><NuxtLink to="/"> Home </NuxtLink></li>
+      <li><NuxtLink to="/about"> About </NuxtLink></li>
+      <li><NuxtLink to="/info"> Info </NuxtLink></li>
+      <li><NuxtLink to="/page/1"> page 1 </NuxtLink></li>
+      <li><NuxtLink to="/page/2"> page 2 </NuxtLink></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.nav {
+  background-color: #cccccc;
+
+  width: 100%;
+  height: 50px;
+
+  padding: 15px;
+
+  ul {
+    width: 100%;
+    height: 100%;
+
+    list-style: none;
+
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+    align-items: center;
+
+    li {
+      a {
+        color: #000000;
+
+        text-decoration: none;
+
+        &:hover {
+          opacity: 0.8;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
